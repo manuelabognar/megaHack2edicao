c8fe461e4787d1c7de910a141e4a8df856d795b1
manuelabognar <manuela.camara@usp.br>
2020-05-02 00:34:54 -0300

    pagina cadastro

--- frontend/src/routes/auth.routes.js
+++ frontend/src/routes/auth.routes.js
@@ -3,13 +3,15 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import SignIn from '../pages/SignIn';
 import About from '../pages/About';
+import Register from '../pages/Register'
 
 export default function AuthRoutes () {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={SignIn} />
-        <Route path="/sobre" component={About} />
+        <Route path="/sobre" component={About} />  
+        <Route path="/cadastro" component={Register} />     
       </Switch>
     </BrowserRouter>
   );
