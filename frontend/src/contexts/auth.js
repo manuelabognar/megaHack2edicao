64111e3e955d7dae9f75aad08b0a472b6b858e4a
Manuela Bognar <manuelabcamara@gmail.com>
2020-05-04 19:14:48 -0300

    Update auth.js

--- frontend/src/contexts/auth.js
+++ frontend/src/contexts/auth.js
@@ -1,8 +1,6 @@
 import React, {createContext, useState, useEffect, useContext} from 'react';
 import api from '../services/api';
 import * as auth from '../services/auth';
-import AuthSolicitante from './authSolicitante';
-import AuthCartorio from './authCartorio';
 
 import AuthSolicitante from './authSolicitante';
 import AuthCartorio from './authCartorio';
